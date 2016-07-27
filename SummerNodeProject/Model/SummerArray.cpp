c0c8865be19a75e809e37fe50fa6f8f18dafca67
tonidavis <cody.henrichsen@740s-pl-114.local>
2016-07-27 10:34:55 -0600

    code to create an array of n length

--- SummerNodeProject/Model/SummerArray.cpp
+++ SummerNodeProject/Model/SummerArray.cpp
@@ -7,3 +7,34 @@
 //
 
 #include "SummerArray.hpp"
+#include <assert.h>
+
+template <class Type>
+SummerArray<Type> :: SummerArray(int length)
+{
+    assert(length > 0);
+    
+    this->length = length;
+    modifiedNodes = new bool[length];
+    front = nullptr;
+    end = nullptr;
+    
+    DataNode<Type> * start = new DataNode<Type>();
+    front = start;
+    end = start;
+    
+    for (int index = 1; index < length; index++)
+    {
+        DataNode<Type> * next = new DataNode<Type>();
+        end-> setNodePointer(next);
+        end = next;
+        
+    }
+    
+    for(int index = 0; index < length; index++)
+    {
+        modifiedNodes[index] = false;
+    }
+    
+}
+    
