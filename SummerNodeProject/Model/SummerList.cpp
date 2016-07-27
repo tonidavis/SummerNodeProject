0d3b2ab6d8e2fd09b71b5d112544d95a149f5946
tonidavis <cody.henrichsen@740s-pl-114.local>
2016-07-27 14:36:47 -0600

    add at index for special cases for front and end in cpp for a list

--- SummerNodeProject/Model/SummerList.cpp
+++ SummerNodeProject/Model/SummerList.cpp
@@ -23,23 +23,43 @@ void SummerList<Type>  :: addAtIndex(int index, Type data)
     assert(index >= 0 && index <= size);
     //different from array, we can add to the end.
     DataNode<Type> * indexPointer = front;
-    
-   //get to the spot before where you want to add the data
-    for(int position = 0; position < index-1; position++)
-    {
-        indexPointer = indexPointer->getNodePointer();
-    }
-    
+  
     //create node
     DataNode<Type> * newNode= new DataNode<Type>(data);
     
-    newNode->setNodePointer(indexPointer->getNodePointer());
-    //next 2 lines do same as previous 1 line
-    //DataNode<Type> * temp = indexPointer->getNodePointer();
-    //newNode->setNodePointer(temp);
+    if(index == 0)
+    {
+        newNode->setNodePointer(indexPointer);
+        front = newNode;
+    }
     
+    else if(index == size-1)
+    {
+        end->setNodePointer(newNode);
+        end = newNode;
+    }
     
+    else
+    {
+    
+        //get to the spot before where you want to add the data
+        for(int position = 0; position < index-1; position++)
+        {
+            indexPointer = indexPointer->getNodePointer();
+        }
+        
+        //pointed newNode to the address of indexPointers next node
+        //newNode->setNodePointer(indexPointer->getNodePointer());
+        //next 2 lines do same as previous 1 line
+        DataNode<Type> * temp = indexPointer->getNodePointer();
+        newNode->setNodePointer(temp);
+        
+        //setting indexPointer to point the newNodes address
+        indexPointer->seNodePointer(newNode);
+    
+    }
+    
+}
    // DataNode<Type> * indexPointer = end;
     
     
-}
