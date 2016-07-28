78e9cf72162aca5de09f4e00ca8edb51241832cc
tonidavis <cody.henrichsen@740s-pl-114.local>
2016-07-28 09:51:39 -0600

    added getters and setters to .cpp binarytreenode

--- SummerNodeProject/Model/BinaryTreeNode.cpp
+++ SummerNodeProject/Model/BinaryTreeNode.cpp
@@ -7,3 +7,63 @@
 //
 
 #include "BinaryTreeNode.hpp"
+
+template <class Type>
+//constructor
+BinaryTreeNode<Type> :: BinaryTreeNode() : DataNode<Type>()
+{
+   //initialize
+    parent = nullptr;
+    leftChild = nullptr;
+    rightChild = nullptr;
+}
+
+//value based constructor
+template <class Type>
+//pass data parameter to super class constructor
+BinaryTreeNode<Type> :: BinaryTreeNode(Type data) : DataNode<Type>(data)
+{
+    //initialize
+    //this->setNodeData(data); This is not needed because of "DataNode<Type>(data)" above
+    parent = nullptr;
+    leftChild = nullptr;
+    rightChild = nullptr;
+    
+}
+
+//getters and setters
+template <class Type>
+BinaryTreeNode<Type> * BinaryTreeNode<Type> ::  getLeftChild()
+{
+    return leftChild;
+}
+
+template <class Type>
+void BinaryTreeNode<Type> :: setLeftChild(BinaryTreeNode<Type>  * leftChild)
+{
+    this->leftChild = leftChild;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * BinaryTreeNode<Type> ::  getRightChild()
+{
+    return rightChild;
+}
+
+template <class Type>
+void BinaryTreeNode<Type> :: setRightChild(BinaryTreeNode<Type>  * rightChild)
+{
+    this->rightChild = rightChild;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * BinaryTreeNode<Type> ::  getParent()
+{
+    return parent;
+}
+
+template <class Type>
+void BinaryTreeNode<Type> :: setParent(BinaryTreeNode<Type>  * parent)
+{
+    this->parent = parent;
+}
